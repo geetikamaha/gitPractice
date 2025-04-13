24f1e184e4a625befedf539955ccf8f73077666a
Geetika Mahajan <geetika.mahajan16@gmail.com>
2025-04-13 16:38:27 -0700

    Initial Commit

---
+++ gitPractice/gitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPracticeApp.swift
+//  gitPractice
+//
+//  Created by Geetika Mahajan on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct gitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
